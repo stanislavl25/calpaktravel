a1e5068ad15d725ed89ed9f023b3340f86370c6b
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-05-18 05:55:02 +0000

    Update from Shopify for theme Testing | Giovanny branch
    Committed from shop: CALPAK

--- assets/checkout.dates.js
+++ assets/checkout.dates.js
@@ -126,7 +126,7 @@ if(Shopify.Checkout.step == "shipping_method") {
     
     targetElement.insertAdjacentElement("afterend", shippingMessageElement);
     targetElement.insertAdjacentElement("afterend", shippingInfoElement);
-  }, 500); // Change the delay (in milliseconds) as needed
+  }, 200); // Change the delay (in milliseconds) as needed
 }
 
 document.addEventListener('page:load', function() {
